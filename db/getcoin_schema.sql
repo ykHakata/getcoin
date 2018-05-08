c5f71a59ea97137806b0ee743d9b75f7da7f89c4
yk <yosizuka1@gmail.com>
2018-05-08 15:32:37 +0900

    add sql

--- db/getcoin_schema.sql
+++ db/getcoin_schema.sql
@@ -1,10 +1,38 @@
 DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-    id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    login_id        TEXT,
-    password        TEXT,
-    approved        INTEGER,
-    deleted         INTEGER,
-    created_ts      TEXT,
-    modified_ts     TEXT
+CREATE TABLE user (                                     -- ユーザー
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,  -- ID (例: 5)
+    login_id        TEXT,                               -- ログインID名 (例: 'getcoin@gmail.com')
+    password        TEXT,                               -- ログインパスワード (例: 'getcoin')
+    approved        INTEGER,                            -- 承認フラグ (例: 0: 承認していない, 1: 承認済み)
+    deleted         INTEGER,                            -- 削除フラグ (例: 0: 削除していない, 1: 削除済み)
+    created_ts      TEXT,                               -- 登録日時 (例: '2016-01-08 12:24:12')
+    modified_ts     TEXT                                -- 修正日時 (例: '2016-01-08 12:24:12')
+);
+DROP TABLE IF EXISTS limitation;
+CREATE TABLE limitation (                               -- 制限
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,  -- ID (例: 5)
+    user_id         INTEGER,                            -- ユーザーID (例: 5)
+    status          INTEGER,                            -- ステータス (例: 1**: 管理者, 2**: 一般)
+    deleted         INTEGER,                            -- 削除フラグ (例: 0: 削除していない, 1: 削除済み)
+    created_ts      TEXT,                               -- 登録日時 (例: '2016-01-08 12:24:12')
+    modified_ts     TEXT                                -- 修正日時 (例: '2016-01-08 12:24:12')
+);
+DROP TABLE IF EXISTS profile;
+CREATE TABLE profile (                                  -- プロフィール
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,  -- ID (例: 5)
+    user_id         INTEGER,                            -- ユーザーID (例: 5)
+    name            TEXT,                               -- 名前 (例: '松野 おそ松')
+    rubi            TEXT,                               -- ふりがな (例: 'まつの おそまつ')
+    nickname        TEXT,                               -- ニックネーム (例: 'おそまつ')
+    email           TEXT,                               -- Eメール (例: 'getcoin@gmail.com')
+    tel             TEXT,                               -- 電話番号 (例: '090-0000-2222')
+    gender          INTEGER,                            -- 性別 (例: 1: 男性, 2: 女性)
+    icon            TEXT,                               -- アイコン画像 (例: 'icon.jpg')
+    birthday        TEXT,                               -- 生年月日 (例: '1974-05-20')
+    zipcode         TEXT,                               -- 郵便番号 (例: '812-8577')
+    address         TEXT,                               -- 住所 (例: '福岡県福岡市博多区東公園7番7号')
+    address_map     TEXT,                               -- 住所の地図情報 (例: '35.7014561,139.7446664,16')
+    deleted         INTEGER,                            -- 削除フラグ (例: 0: 削除していない, 1: 削除済み)
+    created_ts      TEXT,                               -- 登録日時 (例: '2016-01-08 12:24:12')
+    modified_ts     TEXT                                -- 修正日時 (例: '2016-01-08 12:24:12')
 );
